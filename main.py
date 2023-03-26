20111c08f51d537e9166f3b02c82aabb4b2ad322
Steven A Colfin <steve@macbook-pro.lan>
2023-03-26 14:50:03 -0400

    First check in for calculator lesson

---
+++ main.py
@@ -0,0 +1,36 @@
+## What do we want to learn today?
+# -- Aleksander's answer: We want to learn how to use a calculator. Not how to use! You are serously typing everything i say?!
+# We want to learn to build a simple calculator.
+# In this we will learn:
+# 1 how to read from the console
+# 2 How to print stuff out.
+# 3 If statements
+# 4 Maybe a loop :)
+
+def printExample():
+    """
+    This is a function that shows how printing stuff works in python!
+    """
+    pass
+
+def readExample():
+    """
+    This is an example of a function that reads stuff in from the console
+    """
+    pass
+
+def ifStatementExample():
+    """
+    This is an example of how if statements work
+    """
+    pass
+
+def main():
+    """
+    This is where you (Aleksander) will code the calculator!!
+    :return: Awesomeness!!
+    """
+    pass
+
+if __name__ == "__main__":
+    main()
